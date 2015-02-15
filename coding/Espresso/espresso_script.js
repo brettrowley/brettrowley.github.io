77e959a2788c7ed55d3d3d9597d27943902ebe9b
Brett Rowley <browley@rapidminer.com>
2015-02-15 15:27:21 -0500

    Cleaning up Espresso!

--- coding/Espresso/espresso_script.js
+++ coding/Espresso/espresso_script.js
@@ -1,3 +1,4 @@
+//define the variables to be used
 var x = document.getElementById("espresso");
 var lat_coor;
 var long_coor;
@@ -11,6 +12,7 @@ var results_counter = 0;
 var results_list =[]
 var winners_id = []
 
+//define function to get current location
 function getLocation() {
 	if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -18,72 +20,37 @@ function getLocation() {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
+//run this function on page load
+$(document).ready(getLocation)
+
+//define function to print current position (for debugging)
 function showPosition(position) {
     lat_coor = 	position.coords.latitude;
     long_coor = position.coords.longitude;
     console.log(lat_coor, long_coor)
 }
 
-$(document).ready(getLocation)
 
 $(document).ready(function(){
-	var viewportWidth = $(window).width();
-	var viewportHeight = $(window).height();
-  //SCALABILITY FOR MOBILE VS DESKTOP
-	if (viewportWidth < 700){
-		$("h1").css("font-size", "2.5em")
-		$("h1").html("Espresso <br /> Patronum!")
-		$(".action").css("margin", '10px 5px')
-		$("#header_box").css("padding", "5px")
-	} else {
-		$("h1").css("font-size", "4em")
-		$(".action").css("margin", "10px 20px")
-		$("#header_box").css("padding", "15px")
-	}
-//SCALABILITY ON RESIZE
-$(window).resize(function(){
-	var viewportWidth = $(window).width();
-	var viewportHeight = $(window).height();
-
-	if (viewportWidth < 700){
-		$("h1").css("font-size", "2.5em")
-		$("h1").html("Espresso <br /> Patronum!")
-		$(".action").css("margin", "10px 5px")
-		$("#header_box").css("padding", "5px")
-	} else {
-		$("h1").css("font-size", "4em")
-		$("h1").html("Espresso Patronum!")
-		$(".action").css("margin", "10px 20px")
-		$("#header_box").css("padding", "15px")
-	}
-})
-
-$("#refresh_info").hide()
-$("#result").hide()
-$("#error").hide()
-$("#searching").hide()
+//hide results page by default
+$('#results-page').hide()
 	
-function initialize() {
-	//ASSIGNS LAT/LONG COORDINATES TO A VARIABLE
-	
-  // var nearby = new google.maps.LatLng(42.373616 , -71.109734); //FOR LOCAL TESTS
+//define function to create google maps ap
+function initialize() {	
+  // var nearby = new google.maps.LatLng(42.373616 , -71.109734); //FOR LOCAL SERVER TESTS
   var nearby = new google.maps.LatLng(lat_coor, long_coor);
   
-  	//GENERATES DETAILS FOR THE MAP CANVAS
-    map = new google.maps.Map(document.getElementById('map-canvas'), {
-      center: nearby,
-      zoom: 15
-    });
+  	//generates a map (required for api to function)
+    map = new google.maps.Map(document.getElementById('map-canvas'));
   
-    //REQUEST OBJECT WITH ITEMS
+    //specify options in request for cafes with the keyword 'coffee' in their description,
+    //ideally within 500 meters of the current location
     var request = {
       location: nearby,
       radius: 500,
       keyword: "coffee",
       types:['cafe'],
       openNow: true
-  
-  
     };
     infowindow = new google.maps.InfoWindow();
     var service = new google.maps.places.PlacesService(map);
@@ -91,34 +58,37 @@ function initialize() {
   }
   
   function callback(results, status) {
+    //creates a list of all results returned by api
     results_list = results
-    console.log("Got " + results_list.length + " results!")
+    // console.log("Got " + results_list.length + " results!") //log how many results were returned
 
   	var ratings_array=[]
   	var ratings=[]
   
-  	//CONVERTS NON-EXISTENT RATINGS TO '0'
+  	//convert results without ratings to 'rating = 0'
   	for (var i=0; i < results.length; i++) {
   		if (typeof results[i].rating == "undefined"){
   			results[i].rating = 0;
   		}
   	}
-  	//CREATES ARRAY WITH RESULTS INDEIX AND RATING
+  	//create an array with the index value of the result and the result's rating
   	for (var i=0; i<results.length; i++) {
   		var temp = {id: i, rating: results[i].rating}
   		ratings_array.push(temp)
   		ratings.push(results[i].rating)
   	}
-  
-  	var pool = ratings.sort() //SORTS ALL RESULTS BY RATING
+    //sort the index+rating array by rating
+  	var pool = ratings.sort()
 
+    //set the number of results to display back to the user
     var display_num = 6;
 
+    //set the maximum number equal to the number of results if the results list is "small"
     if (display_num > pool.length){
       display_num = pool.length
     }
 
-  	var min_rating = pool[pool.length - display_num] //DEFINES THE THRESHOLD FOR RESULT RATINGS
+  	var min_rating = pool[pool.length - display_num]
   
   	for (var i=0; i < ratings_array.length; i++){
   		if (ratings_array[i].rating >= min_rating){ //IF A RESULT HAS A RATING HIGHER THAN THE THRESHOLD, APPEND IT TO THE ARRAY
@@ -141,32 +111,25 @@ function initialize() {
     }
     $("#name").html(store)
     $("#address").html(city[0])
-    $("#directions").html("<a target='_blank' href='http://www.maps.google.com/?q=" + store + " " + address + "'><img id='map_icon' src='Images/directions.svg' /></a>")
-    $("#yelp").html("<a target='_blank' href='http://www.yelp.com/search?find_desc=" + store + "&find_loc=" + city[1] + "'><img id='yelp_icon' src='Images/yelp.svg' /></a>")
-	  $("#result").fadeIn();
+    $("#directions").attr("href", "http://www.maps.google.com/?q=" + store + " " + address)
+    $("#yelp").attr("href", "http://www.yelp.com/search?find_desc=" + store + "&find_loc=" + city[1])
+	  $("#results-page").fadeIn();
 
-    $("#city").html("<u>" + city[1] + "</u>");
-    $("#searching").fadeIn();
+    $("#city").html("'" + city[1].replace(" ","") + "'");
   }
   ////////////////////////////////////
   $("#espresso").click(function(){ //DEFINES WHAT HAPPENS WHEN THE INITIAL ESPRESSO BUTTON IS PRESSED
-    $(".help_text").fadeOut(600) //FADE THE INFO ICON
   	getLocation //CALL GEOLOCATION
   	if (String(lat_coor)  == 'undefined'){ //SHOW ERROR MESSAGE IF LOCATION CANNOT BE FETCHED
   		$("#error").fadeIn()
   		console.log("Could not get current location")
   		return;
   	}
-  	$(this).css("transition", "none").fadeOut(600) //FADE OUT THE BUTTON
     $("#error").fadeOut() //FADE OUT THE ERROR, IF APPLICABLE
+    $('#front-page').fadeOut()
   	setTimeout(initialize, 800) //CALL GOOGLE MAPS AFTER 800 MS
   })
 ////////////////////////////////////
-  $("#city").click(function(){
-    $("#result").fadeOut(500)
-    results_counter = 0;
-    initialize()
-  })
 
   $("#refresh").click(function(){
   	$("#result").fadeOut(500)
