b80bdd69193f500f649454247732f042af96d16f
Brett Rowley <browley@rapidminer.com>
2016-02-11 18:35:19 -0500

    Whole new style!

---
+++ sandbox/wellable/data-array.js
@@ -0,0 +1,2 @@
+var data = ['The Young and the Rest of Us','564.2','Unbeatable Unicorns','558.3','HASFAL','376.7','S.M.A.R.T@ DE','170.3','London Pride','162.6','Rogue Nation','157.5','']
+var hourRun = 23
