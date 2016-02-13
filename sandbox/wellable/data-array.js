36454f2a23ec6954308ec54dbcd4da1d18d1f7bb
Brett Rowley <browley@rapidminer.com>
2016-02-13 16:08:16 -0500

    Manually push data

--- sandbox/wellable/data-array.js
+++ sandbox/wellable/data-array.js
@@ -1,2 +1,2 @@
-var data = ['The Young and the Rest of Us','1,304.9','Unbeatable Unicorns','1,006.3','HASFAL','789.0','London Pride','410.2','S.M.A.R.T@ DE','318.3','Rogue Nation','252.6','']
-var hourRun = 17
+var data = ['The Young and the Rest of Us','1,349.6','Unbeatable Unicorns','1,065.2','HASFAL','824.5','London Pride','424.6','S.M.A.R.T@ DE','318.3','Rogue Nation','287.2','']
+var hourRun = 20
